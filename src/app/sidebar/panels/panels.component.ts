8d7eda6c7377a90bc4fbe1f8d95284ed1744c3d1
Aanchal Upadhayay <uaanchal551999@gmail.com>
2021-04-07 14:18:53 +0530

    update profile and loogin

--- src/app/sidebar/panels/panels.component.ts
+++ src/app/sidebar/panels/panels.component.ts
@@ -8,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class PanelsComponent implements OnInit {
 
   constructor() { }
+  
+ // userList:Array<any> = JSON.parse(localStorage.getItem('Users')||'{}');
+  //username:string = (localStorage.getItem('username')||'{}');
 
-  userList:Array<any> = JSON.parse(localStorage.getItem('Users')||'{}');
-  username:string = (localStorage.getItem('username')||'{}');
+ 
+  users = JSON.parse(localStorage.getItem('username')||'{}');
+  user:any={};
+   userList:any=[{'f1':this.users.f1,'l1':this.users.l1,'e1':this.users.e1,'f2':this.users.f2,'a1':this.users.a1,'p1':this.users.p1,'p2':this.users.p2}];
+ 
+   onSubmit()
+  {
+    if(localStorage.getItem(this.users.e1))
+    {
+      localStorage.setItem(this.users.e1,JSON.stringify(this.userList));
+     // localStorage.setItem('currentUser',JSON.stringify(this.userList));
+      //users=JSON.parse(localStorage.getItem('Users')!);
+      //users=[user,...users];//...is a spread operator that allows elements of array to expand in existing array
+      alert("Updated Profile!!")
+    }
+    else
+    {
+      alert("Not Updated!!")
+    }
+  }
 
+ 
+ 
+  
+  
+  
+  
+  
+  
   ngOnInit(): void {
   }
   form_el=[{'item1':'','item2':'','item3':'','item4':'','item5':'','item6':'','item7':'','item8':'Himachal Pradesh','item9':'','item10':'','item11':''}]
